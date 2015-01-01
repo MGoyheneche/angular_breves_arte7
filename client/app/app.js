b9159f3954fb8e405eecf9075e1283bc76fbb7f6
Mathias Goyheneche <mathias.goyheneche@gmail.com>
2015-01-01 23:42:40 +0100

    added some style and scroll to directive

--- client/app/app.js
+++ client/app/app.js
@@ -18,15 +18,29 @@ angular.module('brevesApp').config(RouteConfig);
 
 
 // TODO: moove into a Service, a directive ?
-angular.module('brevesApp').run(function($rootScope, $timeout) {
+angular.module('brevesApp').run(function($rootScope, $timeout, $location) {
   $rootScope.modalAlert = function(alert){
     console.log(alert);
     $rootScope.alert = alert;
     $timeout(function() {
       $rootScope.alert = {};
     }, alert.duration || 5000);
-  }
+  };
+
+  // $rootScope.gotoAnchor = function(hash) {
+  //   if ($location.hash() !== hash) {
+  //     // set the $location.hash to `newHash` and
+  //     // $anchorScroll will automatically scroll to it
+  //     console.log("hello");
+  //     $location.hash(hash);
+  //   } else {
+  //     // call $anchorScroll() explicitly,
+  //     // since $location.hash hasn't changed
+  //     $anchorScroll();
+  //   }
+  // };
 });
+
 
 
 // $(function(){
