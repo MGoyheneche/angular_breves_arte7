43d8b77505b7bd9e6a87d53d56b1b8121ef46396
Mathias Goyheneche <mathias.goyheneche@gmail.com>
2015-01-02 20:56:24 +0100

    before correction

--- dist/public/app/components/breves_mail/brevesMailDirective.js
+++
@@ -1,9 +0,0 @@
-function brevesMailDirective() {
-  return {
-    restrict: 'E',
-    templateUrl: 'app/components/breves_mail/brevesMailView.html',
-    controller: 'BrevesMailCtrl'
-  };
-}
-
-angular.module('brevesApp').directive('brevesMail', brevesMailDirective);
